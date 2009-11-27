f3f5767ba2841702bae5fba55d7ab8612fe89ce6
Dag Olav Prestegarden <dagolav@prestegarden.com>
2009-11-27 21:18:47 +0000

    Adding some forgotten file concerning logging.

---
+++ source/APD.Client.Web/Services/LogEntryRepositoryService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+using System.Text;
+
+using APD.DomainModel.Framework;
+using APD.DomainModel.Framework.Logging;
+using APD.Integration.Database.DomainModel.Repositories;
+
+
+namespace APD.Client.Web.Services
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceKnownType(typeof(LogEntry))]
+    [ServiceKnownType(typeof(ErrorLogEntry))]
+    [ServiceKnownType(typeof(WarningLogEntry))]
+    [ServiceKnownType(typeof(InfoLogEntry))]
+    [ServiceKnownType(typeof(Specification<LogEntry>))]
+    [ServiceKnownType(typeof(AllSpecification<LogEntry>))]
+    public class LogEntryRepositoryService
+    {
+
+        LogEntryDatabaseRepository repo = new LogEntryDatabaseRepository();
+
+        [OperationContract]
+        public void Log(LogEntry logEntry)
+        {
+            repo.Save(logEntry);
+        }
+
+        [OperationContract]
+        public IEnumerable<LogEntry> Get(Specification<LogEntry> specification)
+        {
+            return repo.Get(specification);
+        }
+    }
+}
---
+++ source/APD.DomainModel/Logging/LogEntry.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APD.DomainModel.Logging
+{
+    public abstract class LogEntry
+    {
+        public virtual string Source { get; protected set; }
+        public virtual string Message { get; protected set; }
+        public virtual int Severity { get; protected set; }
+        public virtual DateTime TimeStamp { get; set; }
+
+        protected LogEntry() { }
+
+        public LogEntry(string source, string message)
+        {
+            this.Source = source;
+            this.Message = message;
+            this.TimeStamp = DateTime.Now;
+        }
+        public LogEntry(string source, string message, DateTime timeStamp)
+        {
+            this.Source = source;
+            this.Message = message;
+            this.TimeStamp = timeStamp;
+        }
+
+        public virtual new bool Equals(object obj)
+        {
+            return this.GetHashCode().Equals(obj.GetHashCode());
+        }
+
+        public virtual new int GetHashCode()
+        {
+            return string.Format("{0}.{1}", TimeStamp, Message).GetHashCode();
+        }
+    }
+
+    #region Log classes
+    public class InfoLogEntry : LogEntry
+    {
+        public InfoLogEntry() { }
+
+        public InfoLogEntry(string source, string message)
+            : base(source, message)
+        {
+            this.Severity = 2;
+        }
+    }
+
+    public class WarningLogEntry : LogEntry
+    {
+        public WarningLogEntry() { }
+
+        public WarningLogEntry(string source, string message)
+            : base(source, message)
+        {
+            this.Severity = 1;
+        }
+    }
+
+    public class ErrorLogEntry : LogEntry
+    {
+        public ErrorLogEntry() { }
+        public ErrorLogEntry(string source, string message)
+            : base(source, message)
+        {
+            this.Severity = 0;
+        }
+    }
+    #endregion
+}
