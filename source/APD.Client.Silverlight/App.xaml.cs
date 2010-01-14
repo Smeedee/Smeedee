97d9f31ce3f032d4e8248630a2cb09dc9bbd6558
Ole-André Johansen <oleandrejohansen@gmail.com>
2010-01-14 09:33:57 +0000

    Trying to fix the broken build on the build server by adding all NHibernate references

--- source/APD.Client.Silverlight/App.xaml.cs
+++ source/APD.Client.Silverlight/App.xaml.cs
@@ -72,23 +72,10 @@ namespace APD.Client.Silverlight
             });
         }
 
-        private void Application_Exit(object sender, EventArgs e) {}
+        private void Application_Exit(object sender, EventArgs eventArgs) {}
 
         private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
         {
-            // If the app is running outside of the debugger then report the exception using
-            // the browser's exception mechanism. On IE this will display it a yellow alert 
-            // icon in the status bar and Firefox will display a script error.
-            if (!System.Diagnostics.Debugger.IsAttached)
-            {
-                // NOTE: This will allow the application to continue running after an exception has been thrown
-                // but not handled. 
-                // For production applications this error handling should be replaced with something that will 
-                // report the error to the website and stop the application.
-                e.Handled = true;
-                Deployment.Current.Dispatcher.BeginInvoke(delegate { ReportErrorToDOM(e); });
-            }
-
             try
             {
                 ILog logger = new DatabaseLogger(new LogEntryWebservicePersister());
@@ -100,14 +87,23 @@ namespace APD.Client.Silverlight
                 };
 
                 logger.WriteEntry(error);
-
+                e.Handled = true;
             }
-            catch (Exception)
+            catch (Exception logException)
             {
-                // We ignore errors to keep application alive
+                // If the app is running outside of the debugger then report the exception using
+                // the browser's exception mechanism. On IE this will display it a yellow alert 
+                // icon in the status bar and Firefox will display a script error.
+                if (!System.Diagnostics.Debugger.IsAttached)
+                {
+                    // NOTE: This will allow the application to continue running after an exception has been thrown
+                    // but not handled. 
+                    // For production applications this error handling should be replaced with something that will 
+                    // report the error to the website and stop the application.
+                    e.Handled = true;
+                    Deployment.Current.Dispatcher.BeginInvoke(delegate { ReportErrorToDOM(e); });
+                }
             }
-            
-
         }
 
         private void ReportErrorToDOM(ApplicationUnhandledExceptionEventArgs e)
