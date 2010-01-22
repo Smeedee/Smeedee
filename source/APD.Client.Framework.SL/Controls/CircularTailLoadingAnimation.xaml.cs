b7ddb412275fc7909050ed41cf78c41a8e446301
Daniel Nyvik <d.nyvik@gmail.com>
2010-01-22 15:40:00 +0000

    New "standard" loading animation. I figured we could depate placement colors and form next Thursday.

---
+++ source/APD.Client.Framework.SL/Controls/CircularTailLoadingAnimation.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace APD.Client.Framework.SL.Controls
+{
+    public partial class CircularTailLoadingAnimation : UserControl
+	{
+        public CircularTailLoadingAnimation()
+		{
+            InitializeComponent();
+
+            VisualStateManager.GoToState(this, "Animating", true);
+		}
+	}
+}
