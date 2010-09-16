da02b7ef957a3bc3e23fbe4c694a59e69f18ed2e
Ole-André Johansen <oleandrejohansen@gmail.com>
2010-09-16 10:21:52 +0000

    Added a service for the WidgetMetadata

---
+++ source/Client/Smeedee.Client.Web/Services/WidgetMetadataRepositoryService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using Smeedee.DomainModel.Config.SlideConfig;
+using Smeedee.DomainModel.Framework;
+
+namespace Smeedee.Client.Web.Services
+{
+    [ServiceContract(Namespace = "http://smeedee.org")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class WidgetMetadataRepositoryService
+    {
+        [OperationContract]
+        public IEnumerable<WidgetInfo> Get(Specification<WidgetInfo> specification )
+        {
+            return new List<WidgetInfo>();
+        }
+    }
+}
