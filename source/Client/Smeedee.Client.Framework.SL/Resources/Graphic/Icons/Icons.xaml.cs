5db8412a7f70d92d057e517fbc284a7493c2cf95
Gøran Hansen <mail@goeran.no>
2010-09-21 05:50:34 +0000

    Added icon for fullscreen.

---
+++ source/Client/Smeedee.Client.Framework.SL/Resources/Graphic/Icons/Icons.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Smeedee.Client.Framework.SL
+{
+	public partial class Icons : UserControl
+	{
+		public Icons()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
