0cf783ac71275b24963e3ed7360c2c4ca12ed624
Gøran Hansen <mail@goeran.no>
2010-09-23 06:17:25 +0000

    Added icon for TaskAdministration and adjusted the size of the AdminDockBar

--- source/Client/Smeedee.Client.Framework/Services/Impl/ModuleLoader.cs
+++ source/Client/Smeedee.Client.Framework/Services/Impl/ModuleLoader.cs
@@ -211,6 +211,7 @@ namespace Smeedee.Client.Framework.Services.Impl
         {
 #if SILVERLIGHT
             if (title == "Holidays") return new HolidaysIcon();
+            if (title == "Task Administration") return new TaskAdministrationIcon();
             return new SettingsIcon(title);
 #endif
 
