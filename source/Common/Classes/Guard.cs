640163b9a3b1368c13b9528cefe2dd336447296a
Gøran Hansen <mail@goeran.no>
2010-09-28 05:37:16 +0000

    Removed the Guard project and added the Guard class to the Solution. Dependent project now just link to this file

---
+++ source/Common/Classes/Guard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Smeedee.Framework
+{
+    public static class Guard
+    {
+		public static void Requires<T>(bool expression) where T : Exception
+		{
+			if (expression == false)
+				throw Activator.CreateInstance<T>();
+		}
+
+        public static void Requires<T>(bool expression, string message) where T : Exception
+        {
+            if (expression == false)
+                throw Activator.CreateInstance(typeof (T), new object[] {message}) as T;
+        }
+    }
+}
