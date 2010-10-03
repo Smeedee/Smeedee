18810d8e601a714240fe13df127b5d915e58b895
Bente Cecilie Andorsen <benteca@gmail.com>
2010-10-03 16:51:38 +0000

    - The "Show in fullscreen"-button now changes its tooltip text to "Exit fullscreen" when showing in fullscreen. - The button also works both ways when in out-of-browser.

--- source/Client/Smeedee.Client.Framework/ViewModel/DockBarItems/FullScreen.cs
+++ source/Client/Smeedee.Client.Framework/ViewModel/DockBarItems/FullScreen.cs
@@ -9,17 +9,32 @@ namespace Smeedee.Client.Framework.ViewModel.DockBarItems
 {
     public class FullScreen : DockBarItem
     {
+        private bool isInFullScreen;
+
         public FullScreen()
         {
+            isInFullScreen = false;
             Description = "Show in fullscreen";
             Click = new DelegateCommand(() =>
             {
                 var fullScreenService = this.GetDependency<IFullScreenService>();
-                fullScreenService.GoIntoFullScreen();
+                if (isInFullScreen)
+                {
+                    fullScreenService.GoOutFromFullScreen();
+                    isInFullScreen = false;
+                    Description = "Show in fullscreen";
+                }
+                else
+                {
+                    fullScreenService.GoIntoFullScreen();
+                    isInFullScreen = true;
+                    Description = "Exit fullscreen";
+                }
             });
 #if  SILVERLIGHT
             Icon = new FullScreenIcon();
-#endif
+#endif     
+            
         }
     }
 }
