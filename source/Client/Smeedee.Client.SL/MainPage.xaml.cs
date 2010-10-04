2d1ead53c81368a29633d6780f7cb54f1fc8472f
Bente Cecilie Andorsen <benteca@gmail.com>
2010-10-04 09:34:45 +0000

    The Escape key now exits fullscreen when the application is running out of browser.

--- source/Client/Smeedee.Client.SL/MainPage.xaml.cs
+++ source/Client/Smeedee.Client.SL/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Smeedee.Client.Framework.ViewModel;
+using Smeedee.Client.Framework.ViewModel.DockBarItems;
 
 namespace Smeedee.Client.SL
 {
@@ -7,6 +12,27 @@ namespace Smeedee.Client.SL
         public MainPage()
         {
             InitializeComponent();
+            if(Application.Current.IsRunningOutOfBrowser)
+            {
+                AddHandler(KeyDownEvent, new KeyEventHandler(ExitFullScreenWithEscape), true);            
+            }
+        }
+
+        private void ExitFullScreenWithEscape(object sender, KeyEventArgs keyEvent)
+        {
+            if (keyEvent.Key == Key.Escape && Application.Current.Host.Content.IsFullScreen)
+            {
+                var dockBar = adminDockView.DataContext as DockBar;
+                if (dockBar != null)
+                {
+                    var dockBarItems = dockBar.Items;
+                    foreach (var dockBarItem in dockBarItems.OfType<FullScreen>())
+                    {
+                        dockBarItem.Click.Execute();
+                    }
+                }
+                
+            }
         }
     }
 }
