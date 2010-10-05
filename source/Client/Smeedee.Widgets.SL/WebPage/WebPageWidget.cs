8ebbde25d0c6d3b05ee5b8a3beb3af9e4de2d691
Alex York <alexyork1985@gmail.com>
2010-10-05 20:37:43 +0000

    Added Widget class for web page and views

---
+++ source/Client/Smeedee.Widgets.SL/WebPage/WebPageWidget.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Smeedee.Client.Framework.ViewModel;
+using Smeedee.DomainModel.Config.SlideConfig;
+using Smeedee.Widgets.SL.WebPage.Views;
+using Smeedee.Widgets.WebPage.ViewModel;
+
+namespace Smeedee.Widgets.SL.WebPage
+{
+    [WidgetInfo(Name = "Web Page",
+                Description = "Lets you display a web site",
+                Author = "Smeedee team",
+                Version = "1.0",
+                Tags = new[] { CommonTags.Fun })]
+    public class WebPageWidget : Widget
+    {
+        public WebPageWidget()
+        {
+            var viewModel = new WebPageViewModel();
+
+            View = new WebPageView() {DataContext = viewModel};
+            SettingsView = new WebPageSettingsView() {DataContext = viewModel};
+        }
+    }
+}
