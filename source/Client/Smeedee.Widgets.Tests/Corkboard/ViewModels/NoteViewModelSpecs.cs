4516b6f3b10463ea60393835e394a3067e15446a
Alex York <alexyork1985@gmail.com>
2010-10-10 21:33:09 +0000

    Fixed the build after merging the corkboard widget merge.

---
+++ source/Client/Smeedee.Widgets.Tests/Corkboard/ViewModels/NoteViewModelSpecs.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using Smeedee.DomainModel.Corkboard;
+using Smeedee.Widgets.Corkboard.ViewModel;
+using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.Specification.NUnit;
+
+namespace Smeedee.Widgets.Tests.Corkboard.ViewModels
+{
+    [TestFixture]
+    public class When_move_command_is_triggered : SharedTestFixture
+    {
+        [Test]
+        public void assure_move_event_is_triggered_when_moving_up()
+        {
+            Given(someone_subscribes_to_a_note);
+            When("the note is asking to move up", () => note.MoveUp.ExecuteDelegate());
+            Then("it should raise the Move event exactly once", () => raised.ShouldBe(1));
+        }
+
+        [Test]
+        public void assure_move_event_is_triggered_when_moving_down()
+        {
+            Given(someone_subscribes_to_a_note);
+            When("the note is asking to move down", () => note.MoveDown.ExecuteDelegate());
+            Then("it should raise the Move event exactly once", () => raised.ShouldBe(1));
+        }
+
+        [Test]
+        public void move_down_should_provide_correct_arguments()
+        {
+            Given(someone_subscribes_to_a_note);
+            When("the note is asking to move down", () => note.MoveDown.ExecuteDelegate());
+            Then("the move arguments should specify down as direction", () => moveargs.MoveDirection.ShouldBe(MoveArgs.Direction.DOWN));
+        }
+
+        [Test]
+        public void move_up_should_provide_correct_arguments()
+        {
+            Given(someone_subscribes_to_a_note);
+            When("the note is asking to move up", () => note.MoveUp.ExecuteDelegate());
+            Then("the move arguments should specify up as direction", () => moveargs.MoveDirection.ShouldBe(MoveArgs.Direction.UP));
+        }
+    }
+
+    [TestFixture]
+    public class SharedTestFixture : ScenarioClass
+    {
+        protected static NoteViewModel note;
+        protected static int raised;
+        protected static MoveArgs moveargs;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Scenario("");
+            note = new NoteViewModel { Description = "positive note", Type = NoteType.Positive };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            StartScenario();
+        }
+
+        protected Context someone_subscribes_to_a_note = () =>
+        {
+            raised = 0;
+            note.Move += MoveSubscriber;
+        };
+
+        protected static void MoveSubscriber(object sender, MoveArgs args)
+        {
+            raised++;
+            moveargs = args;
+        }
+    }
+}
