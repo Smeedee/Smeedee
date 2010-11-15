7dab28939d0233578906812a6c3f577dd220edee
Ole-André Johansen <oleandrejohansen@gmail.com>
2010-11-15 22:22:37 +0000

    More tests :)

---
+++ source/DomainModel/Smeedee.DomainModel.Tests/TaskInstanceConfiguration/TaskConfigurationSpecs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Smeedee.DomainModel.TaskInstanceConfiguration;
+using TinyBDD.Specification.NUnit;
+
+namespace Smeedee.DomainModel.Tests.TaskInstanceConfiguration
+{
+    public class TaskConfigurationSpecs
+    {
+        [TestFixture]
+        public class when_reading_entry_value
+        {
+            [Test]
+            public void Assure_null_entries_are_ignored()
+            {
+                var config = new TaskConfiguration();
+                config.Entries.Add(null);
+                config.Entries.Add(new TaskConfigurationEntry(){Name = "testName", Value = "myValue",Type = typeof(string)});
+
+                config.ReadEntryValue("testName").ShouldBe("myValue");
+            }
+        }
+    }
+}
