4515f40fbabbc5e170a5cced9508d6cd23fbeb79
Aleksander S. Larsen <aleksander@ogrim.no>
2011-06-29 15:56:04 +0200

    Added radio buttons in view for selecting scaling mode in TeamPicture #20

--- source/Client/Smeedee.Widgets.SL/TeamPicture/Views/TeamPictureSettingsview.xaml.cs
+++ source/Client/Smeedee.Widgets.SL/TeamPicture/Views/TeamPictureSettingsview.xaml.cs
@@ -8,5 +8,10 @@ namespace Smeedee.Widgets.SL.TeamPicture.Views
         {
             InitializeComponent();
         }
+
+        private void radioButton1_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
     }
 }
