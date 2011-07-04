a4f7baa8c36cf277ae095779c136c6d061a76002
Aleksander S. Larsen <aleksander@ogrim.no>
2011-07-04 16:06:09 +0200

    Started writing tests for new widget #21

---
+++ source/Client/Smeedee.Widgets.Tests/WebSnapshot/ViewModel/WebSnapshotViewModelSpec.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Smeedee.Widgets.Tests.WebSnapshot.ViewModel
+{
+    public class WebSnapshotViewModelSpec
+    {
+
+        [TestFixture]
+        public class When_created : Shared
+        {
+            [Test]
+            public void Then_assure_input_URL_has_some_default_text()
+            {
+                webSnapshotViewModel.InputUrl.ShouldBe("Enter URL here");
+            }
+        }
+
+        public class Shared
+        {
+            protected WebSnapshotViewModel webSnapshotViewModel;
+
+            [SetUp]
+            public void Setup()
+            {
+                webSnapshotViewModel = new WebSnapshotViewModel();
+            }
+
+        }
+    }
+}
