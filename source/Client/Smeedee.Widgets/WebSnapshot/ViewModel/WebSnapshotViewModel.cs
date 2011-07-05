05bf41d3f4fd4e02c1a5b5591b4c807b4fa9143e
Aleksander S. Larsen <aleksander@ogrim.no>
2011-07-05 12:13:04 +0200

    Added files for WebSnapshot widget #21

---
+++ source/Client/Smeedee.Widgets/WebSnapshot/ViewModel/WebSnapshotViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smeedee.Widgets.WebSnapshot.ViewModel
+{
+    public partial class WebSnapshotViewModel
+    {
+
+    }
+}
---
+++ source/DomainModel/Smeedee.DomainModel/WebSnapshot/WebSnapshot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smeedee.DomainModel.WebSnapshot
+{
+    public class WebSnapshot
+    {
+        public virtual string InputUrl { get; set; }
+        public virtual byte[] Picture { get; set; }
+        public virtual int RefreshInterval { get; set; }
+        public virtual int[] CropCoordinates { get; set; }
+    }
+}
