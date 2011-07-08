bcb6e1e3b12cd3d10eaf6533bfe22a44c80d8d0e
Aleksander S. Larsen <aleksander@ogrim.no>
2011-07-08 15:49:32 +0200

    Added forgotten file to unbreak the build #21

---
+++ source/Client/Smeedee.Widgets/WebSnapshot/Util/WebSnapshotter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace Smeedee.Widgets.WebSnapshot.Util
+{
+    public class WebSnapshotter
+    {
+        private string Url { get; set; }
+        public bool IsFinished { get; private set; }
+
+
+        private Bitmap RenderBitmap(WebBrowser wb)
+        {
+            var bitmap = new Bitmap(wb.Width, wb.Height);
+            wb.DrawToBitmap(bitmap, new Rectangle(0, 0, wb.Width, wb.Height));
+            wb.Dispose();
+
+            return bitmap;
+        }
+
+
+
+    }
+}
