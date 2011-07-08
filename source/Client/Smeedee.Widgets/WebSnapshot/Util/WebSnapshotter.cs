a4dd2de1d3d04bb6c4297518de09f5e1487f9e1b
Aleksander S. Larsen <aleksander@ogrim.no>
2011-07-08 15:57:26 +0200

    Removed some code to unbreak the build #21

--- source/Client/Smeedee.Widgets/WebSnapshot/Util/WebSnapshotter.cs
+++ source/Client/Smeedee.Widgets/WebSnapshot/Util/WebSnapshotter.cs
@@ -10,17 +10,5 @@ namespace Smeedee.Widgets.WebSnapshot.Util
         private string Url { get; set; }
         public bool IsFinished { get; private set; }
 
-
-        private Bitmap RenderBitmap(WebBrowser wb)
-        {
-            var bitmap = new Bitmap(wb.Width, wb.Height);
-            wb.DrawToBitmap(bitmap, new Rectangle(0, 0, wb.Width, wb.Height));
-            wb.Dispose();
-
-            return bitmap;
-        }
-
-
-
     }
 }
