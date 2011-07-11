b9e4bda8e9697511e3f14e4210c57bbfe51bb365
Mads Tordal <mads.tordal@gmail.com>
2011-07-11 13:01:55 +0200

    Fixing

--- source/DomainModel/Smeedee.DomainModel/Charting/Chart.cs
+++ source/DomainModel/Smeedee.DomainModel/Charting/Chart.cs
@@ -10,23 +10,23 @@ namespace Smeedee.DomainModel.Charting
     public class Chart
     {
 
-        public virtual string Name { get; set; };
-        public virtual string ChartType { get; set; };
-        public virtual string XAxisName { get; set; };
-        public virtual string YAxisName { get; set; };
-        public virtual IList<DataSet> DataSets { get; set; };
+        public virtual string Name { get; set; }
+        public virtual string ChartType { get; set; }
+        public virtual string XAxisName { get; set; }
+        public virtual string YAxisName { get; set; }
+        public virtual IList<DataSet> DataSets { get; set; }
 
     }
 
     public class DataSet
     {
-        public virtual IList<DataPoint> DataPoints { get; set; };
+        public virtual IList<DataPoint> DataPoints { get; set; }
     }
 
     public class DataPoint
     {
-        public virtual Object X { get; set; };
-        public virtual Object Y { get; set; };
+        public virtual Object X { get; set; }
+        public virtual Object Y { get; set; }
 
     }
 }
