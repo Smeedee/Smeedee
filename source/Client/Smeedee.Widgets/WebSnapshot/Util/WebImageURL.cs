9db0cfd88b9686917146c16da66b8380fe041137
Aleksander S. Larsen <aleksander@ogrim.no>
2011-07-12 12:14:16 +0200

    Added image downloading utility #21

---
+++ source/Client/Smeedee.Widgets/WebSnapshot/Util/WebImageURL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Smeedee.Widgets.WebSnapshot.Util
+{
+    public static class WebImageURL
+    {
+
+        public static Bitmap GetBitmapFromURL(string url)
+        {
+            if (!URLValidator.IsPictureURL(url))
+            {
+                return null;
+            }
+                
+
+            Bitmap picture = null;
+            try
+            {
+                WebRequest request = WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                picture = new Bitmap(stream);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return picture;
+        }
+
+        
+    }
+}
