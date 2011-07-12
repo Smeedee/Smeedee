64e725d52b7d0526db62e4173d4a4d8405e4360a
Kristian Hiim <kristian.hiim@gmail.com>
2011-07-12 12:14:38 +0200

    Added a ChartStorage, used to save chart data in NoSql database. #37

---
+++ source/Integration/Smeedee.Integration.Tests/Database/DomainModel/Charting/ChartStorageSpecs.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Moq;
+using NUnit.Framework;
+using Smeedee.DomainModel.Framework;
+using Smeedee.DomainModel.NoSql;
+using Smeedee.Integration.Database.DomainModel.Charting;
+using Smeedee.DomainModel.Charting;
+using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.Specification.NUnit;
+
+namespace Smeedee.Integration.Tests.Database.DomainModel.Charting
+{
+    class ChartStorageSpecs
+    {
+        [TestFixture]
+        public class When_saving_chart : Shared
+        {
+
+            [Test]
+            [ExpectedException("System.NullReferenceException")]
+            public void Assure_that_trying_to_save_chart_without_database_and_collection_throws_exception()
+            {
+                Given(the_storage_has_been_created).
+                    And(an_empty_chart_has_been_created);
+                When(saving_chart);
+                Then("Expecting an exception");
+
+                Run(); // must run here, because exception will not be caught at the right place otherwise
+            }
+
+            [Test]
+            public void Assure_that_chart_without_data_is_saved()
+            {
+                Given(the_storage_has_been_created).
+                    And(a_chart_without_data_has_been_created);
+                When(saving_chart);
+                Then("Expect a NoSqlDatabase-object to be saved", () => noSqlPersistRepositoryMock.Verify(
+                    a => a.Save(It.Is<NoSqlDatabase>(d => (d.Name == "database" && d.GetCollection("collection").Documents.Count == 0)) )
+                ));
+            }
+
+            [Test]
+            public void Assure_that_chart_with_data_is_saved()
+            {
+                Given(the_storage_has_been_created).
+                    And(a_chart_with_data_has_been_created);
+                When(saving_chart);
+                Then(expect_correct_datastructure);
+            }
+
+
+            private Context an_empty_chart_has_been_created = () =>
+                chart = new Chart();
+
+            
+
+            private Context a_chart_with_data_has_been_created = () =>
+            {
+                chart = new Chart("testbase", "testcol");
+                var set = new DataSet("testset");
+                set.DataPoints.Add(new DataPoint {X = 0, Y = 1});
+                set.DataPoints.Add(new DataPoint {X = 2, Y = 3});
+                chart.DataSets.Add(set);
+                set = new DataSet("test2");
+                set.DataPoints.Add(new DataPoint {X = 42, Y = 42});
+                chart.DataSets.Add(set);
+            };
+
+            private Then expect_correct_datastructure = () =>
+                noSqlPersistRepositoryMock.Verify(
+                    a => a.Save(
+                        It.Is<NoSqlDatabase>(d => ValidateDatastructure(d))));
+
+            private static bool ValidateDatastructure(NoSqlDatabase d)
+            {
+                var collection = d.GetCollection("testcol");
+                var doc0 = collection.Documents[0];
+                var doc1 = collection.Documents[1];
+
+                return d.Name == "testbase" &&
+                       d.Collections.Count == 1 &&
+                       collection.Documents.Count == 2 &&
+                       doc0["Name"].Value<string>() == "testset" &&
+                       doc1["Name"].Value<string>() == "test2" &&
+                       doc0["DataPoints"].Count() == 2 &&
+                       doc1["DataPoints"].Count() == 1 &&
+                       doc0["DataPoints"][0]["X"].Value<int>() == 0 &&
+                       doc0["DataPoints"][0]["Y"].Value<int>() == 1 &&
+                       doc0["DataPoints"][1]["X"].Value<int>() == 2 &&
+                       doc0["DataPoints"][1]["Y"].Value<int>() == 3 &&
+                       doc1["DataPoints"][0]["X"].Value<int>() == 42 &&
+                       doc1["DataPoints"][0]["Y"].Value<int>() == 42;
+            }
+
+            
+        }
+
+        [TestFixture]
+        public class When_database_already_exists : Shared
+        {
+            [Ignore]
+            [Test]            
+            public void Assure_that_database_is_not_overwritten()
+            {
+                Given(the_storage_has_been_created).
+                    And(a_chart_without_data_has_been_created).
+                    And(the_database_already_exists);
+
+            }
+
+            private Context the_database_already_exists = () =>
+                                                              {
+                                                              };
+        }
+
+        public class Shared : ScenarioClass
+        {
+            protected static ChartStorage storage;
+            protected static Chart chart;
+            protected static Mock<IPersistDomainModelsAsync<NoSqlDatabase>> noSqlPersistRepositoryMock;
+            protected static Mock<IAsyncRepository<NoSqlDatabase>> noSqlRepositoryMock;
+
+            protected Context the_storage_has_been_created = () =>
+                storage = new ChartStorage(noSqlPersistRepositoryMock.Object);
+
+            protected Context a_chart_without_data_has_been_created = () =>
+                chart = new Chart("database", "collection");
+
+            protected When saving_chart = () =>
+                storage.Save(chart);
+
+            protected bool hasRun = false;
+
+            public void Run()
+            {
+                if (hasRun) return;
+                hasRun = true;
+                StartScenario();
+            }
+
+            [SetUp]
+            public void SetUp()
+            {
+                noSqlPersistRepositoryMock = new Mock<IPersistDomainModelsAsync<NoSqlDatabase>>();
+                noSqlRepositoryMock = new Mock<IAsyncRepository<NoSqlDatabase>>();
+                storage = null;
+                chart = null;
+                hasRun = false;
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                Run();
+            }
+
+            public static bool Compare(Chart a, Chart b)
+            {
+                if (a.Database != b.Database) return false;
+                if (a.Collection != b.Collection) return false;
+                if (a.DataSets.Count != b.DataSets.Count) return false;
+                for (int i=0; i<a.DataSets.Count; i++)
+                {
+                    if (!CompareDataSet(a.DataSets[i], b.DataSets[i]))
+                        return false;
+                }
+                return true;
+            }
+
+            private static bool CompareDataSet(DataSet a, DataSet b)
+            {
+                if (a.Name != b.Name) return false;
+                if (a.DataPoints.Count != b.DataPoints.Count) return false;
+                for (int i=0; i<a.DataPoints.Count; i++)
+                {
+                    if (!CompareDataPoint(a.DataPoints[i], b.DataPoints[i]))
+                        return false;
+                }
+                return true;
+            }
+            private static bool CompareDataPoint(DataPoint a, DataPoint b)
+            {
+                return (a.X == b.X && a.Y == b.Y);
+            }
+
+        }
+    }
+}
