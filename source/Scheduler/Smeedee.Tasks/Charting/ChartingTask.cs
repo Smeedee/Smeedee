cfa2d5fbe9e3bba0e8a0ed8ded132078a5a998e2
Mads Tordal <mads.tordal@gmail.com>
2011-07-12 16:08:58 +0200

    Fixing broken build.. - hopefully...
    Forgot to add that I started making Task for the generic charting widget.

---
+++ source/Scheduler/Smeedee.Tasks/Charting/ChartingTask.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smeedee.Client.Framework.Services;
+using Smeedee.DomainModel.Charting;
+using Smeedee.DomainModel.Framework;
+using Smeedee.DomainModel.NoSql;
+using Smeedee.DomainModel.TaskInstanceConfiguration;
+using Smeedee.Framework;
+using Smeedee.Integration.Database.DomainModel.Charting;
+using Smeedee.Tasks.Framework;
+using Smeedee.Tasks.Framework.TaskAttributes;
+
+namespace Smeedee.Tasks.Charting
+{
+    [Task("Charting Task",
+         Author = "Smeedee Team",
+         Description = "Retrieves information from some file to show in some chart",
+         Version = 1,
+         Webpage = "http://smeedee.org")]
+    [TaskSetting(1, FILEPATH, typeof(Uri), "")]
+    [TaskSetting(2, VALUE_SEPARATOR, typeof(string), ",","At what value should the data be separated. Ex. CSV uses ','")]
+    [TaskSetting(3, DATABASE_NAME, typeof(string), "", "Give your database a name")]
+    [TaskSetting(4, COLLECTIONS_NAME, typeof(string),"default collection","" )]
+    public class ChartingTask : TaskBase
+    {
+        protected const string FILEPATH = "File path";
+        protected const string VALUE_SEPARATOR = "Value separator";
+        protected const string DATABASE_NAME = "Name of database";
+        protected const string COLLECTIONS_NAME = "Name of collection";
+
+        private IChartStorage chartStorage;
+        private TaskConfiguration _configuration;
+        private IDownloadStringService downloadStringService;
+
+        public ChartingTask(IChartStorage chartStorage, TaskConfiguration configuration, IDownloadStringService downloadStringService)
+        {
+            //Guard.Requires<ArgumentNullException>(databasePersister != null);
+            //Guard.Requires<ArgumentNullException>(configuration != null);
+            //Guard.Requires<TaskConfigurationException>(configuration.Entries.Count() >= 3);
+            this.chartStorage = chartStorage;
+            _configuration = configuration;
+            this.downloadStringService = downloadStringService;
+        }
+
+        public override string Name 
+        { 
+            get { return "Charting Task"; } 
+        }
+
+        public override void Execute()
+        {
+            var separator = (string)_configuration.ReadEntryValue(VALUE_SEPARATOR);
+            var filepath = (string) _configuration.ReadEntryValue(FILEPATH);
+
+            var dataset = GetDataSetFromFile(filepath, separator);
+
+            var chart = new Chart();
+            chart.DataSets.Add(dataset);
+
+            chartStorage.Save(chart);
+            
+        }
+
+        public DataSet GetDataSetFromFile(string somefilepath, string separator)
+        {
+            downloadStringService.DownloadAsync(new Uri(somefilepath), null);
+            
+            return new DataSet();
+        }
+    }
+}
