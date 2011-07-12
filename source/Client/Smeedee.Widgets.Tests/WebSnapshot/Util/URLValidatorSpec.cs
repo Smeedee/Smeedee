30b5e99f0b1f83634215af1e276053a0dbbc8435
Aleksander S. Larsen <aleksander@ogrim.no>
2011-07-12 10:13:09 +0200

    Added tests for URLValidator utility #21

---
+++ source/Client/Smeedee.Widgets.Tests/WebSnapshot/Util/URLValidatorSpec.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TinyBDD.Specification.NUnit;
+using Smeedee.Widgets.WebSnapshot.Util;
+
+namespace Smeedee.Widgets.Tests.WebSnapshot.Util
+{
+    public class URLValidatorSpec
+    {
+
+        [TestFixture]
+        public class When_image_URL_is_given : Shared
+        {
+            [Test]
+            public void Then_assure_it_is_validated_as_image_url()
+            {
+                URLValidator.IsPictureURL(imageURL).ShouldBeTrue();
+            }
+
+            [Test]
+            public void Then_assure_it_is_valid_as_normal_url()
+            {
+                URLValidator.IsValidUrl(imageURL).ShouldBeTrue();
+            }
+        }
+
+        [TestFixture]
+        public class When_page_URL_is_given : Shared
+        {
+            [Test]
+            public void Then_assure_it_is_valid_as_normal_url()
+            {
+                URLValidator.IsValidUrl(url).ShouldBeTrue();
+            }
+
+            [Test]
+            public void Then_assure_it_is_not_valid_image_url()
+            {
+                URLValidator.IsPictureURL(url).ShouldBeFalse();
+            }
+        }
+
+        [TestFixture]
+        public class When_invalid_URL_is_given : Shared
+        {
+            [Test]
+            public void Then_assure_it_is_not_valid_normal_url()
+            {
+                URLValidator.IsValidUrl(invalid).ShouldBeFalse();
+            }
+
+            [Test]
+            public void Then_assure_it_is_not_valid_image_url()
+            {
+                URLValidator.IsPictureURL(invalid).ShouldBeFalse();
+            }
+        }
+
+        public class Shared
+        {
+            protected string imageURL;
+            protected string url;
+            protected string invalid;
+
+            [SetUp]
+            public void Setup()
+            {
+                imageURL = "http://smeedee.org/images/code.png";
+                url = "http://smeedee.org/";
+                invalid = "mister spock is not an url";
+            }
+        
+            
+        }
+    }
+}
