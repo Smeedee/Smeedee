eff71285caae6bb53f9e3954265cdf28a8d7daf8
Kristian Hiim <kristian.hiim@gmail.com>
2011-07-12 14:10:14 +0200

    Updated ChartStorage, so other collections in databases should not be overwritten when saving a chart. #37

--- source/Integration/Smeedee.Integration/Database/DomainModel/Charting/ChartStorage.cs
+++ source/Integration/Smeedee.Integration/Database/DomainModel/Charting/ChartStorage.cs
@@ -11,9 +11,11 @@ namespace Smeedee.Integration.Database.DomainModel.Charting
     public class ChartStorage : IChartStorage
     {
         private IPersistDomainModelsAsync<NoSqlDatabase> persistRepository;
+        private IAsyncRepository<NoSqlDatabase> repository;
 
-        public ChartStorage(IPersistDomainModelsAsync<NoSqlDatabase> persistRepository)
+        public ChartStorage(IAsyncRepository<NoSqlDatabase> repository, IPersistDomainModelsAsync<NoSqlDatabase> persistRepository)
         {
+            this.repository = repository;
             this.persistRepository = persistRepository;
         }
 
@@ -22,14 +24,23 @@ namespace Smeedee.Integration.Database.DomainModel.Charting
             if (chart.Database == null || chart.Collection == null)
                 throw new NullReferenceException();
 
-            var database = new NoSqlDatabase() { Name = chart.Database };
-            var collection = database.GetCollection(chart.Collection);
-            foreach (var dataset in chart.DataSets)
-            {
-                collection.Insert(Document.FromObject(dataset));
-            }
-            persistRepository.Save(database);
+            repository.GetCompleted += (o, e) =>
+                                           {
+                                               var list = e.Result.ToList();
+                                               var database = list.Count > 0 ? list[0] : new NoSqlDatabase {Name = chart.Database};
+
+                                               var collection = database.GetCollection(chart.Collection);
+                                               collection.Documents.Clear();
+                                               foreach (var dataset in chart.DataSets)
+                                               {
+                                                   collection.Insert(Document.FromObject(dataset));
+                                               }
+                                               persistRepository.Save(database);
+                                           };
+
+            repository.BeginGet(new LinqSpecification<NoSqlDatabase>(d => d.Name == chart.Database));
         }
 
+        
     }
 }
