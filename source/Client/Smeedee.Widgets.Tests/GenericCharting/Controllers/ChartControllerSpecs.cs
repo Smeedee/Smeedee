7bcefe42b433950d718575c07cb7f144c2b0ed70
Hege Augland <hege.augland@capgemini.no>
2011-07-13 10:10:52 +0200

    Added two testfiles to test GenericCharting Controllers

---
+++ source/Client/Smeedee.Widgets.Tests/GenericCharting/Controllers/ChartControllerSpecs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TinyBDD.Dsl.GivenWhenThen;
+
+namespace Smeedee.Widgets.Tests.GenericCharting.Controllers
+{
+    class ChartControllerSpecs
+    {
+        [TestFixture]
+        public class When_controller_is_spawned : Shared
+        {
+            [Test]
+            public void Then_something_is_tested()
+            {
+                
+            }
+        }
+        public class Shared : ScenarioClass
+        {
+            
+        }
+    }
+}
