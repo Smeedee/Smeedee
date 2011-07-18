366faa9da99128a251205fe91eb710e75483befa
Kristian Hiim <kristian.hiim@gmail.com>
2011-07-18 10:53:07 +0200

    #32 ChartView now supports lines, columns and areas.

--- source/Client/Smeedee.Widgets/GenericCharting/ViewModels/ChartViewModel.cs
+++ source/Client/Smeedee.Widgets/GenericCharting/ViewModels/ChartViewModel.cs
@@ -9,55 +9,43 @@ using TinyMVVM.Framework.Conventions;
 
 namespace Smeedee.Widgets.GenericCharting.ViewModels
 {
-    public partial class ChartViewModel : AbstractViewModel
+    public class ChartViewModel : AbstractViewModel
     {
 
         public ObservableCollection<DataSetViewModel> Lines { get; set; }
-
-        //State
-        public virtual ObservableCollection<DataPointViewModel> Data
-        {
-            get
-            {
-                OnGetData(ref _Data);
-
-                return _Data;
-            }
-            set
-            {
-                OnSetData(ref value);
-                _Data = value;
-            }
-        }
-
-        private ObservableCollection<DataPointViewModel> _Data;
-        partial void OnGetData(ref ObservableCollection<DataPointViewModel> value);
-        partial void OnSetData(ref ObservableCollection<DataPointViewModel> value);
-
-       
-        
+        public ObservableCollection<DataSetViewModel> Columns { get; set; }
+        public ObservableCollection<DataSetViewModel> Areas { get; set; }
 
         //Commands
         public DelegateCommand Refresh { get; set; }
 
         public ChartViewModel()
         {
+            // Fills collections with sample data, that should be removed when proper loading of data is implemented
             Lines = new ObservableCollection<DataSetViewModel>
                         {
                             new DataSetViewModel {Name = "Test", Data = new ObservableCollection<DataPointViewModel> { new DataPointViewModel {X = 1, Y = 1},  new DataPointViewModel {X = 2, Y = 2}}},
                             new DataSetViewModel {Name = "Test2", Data = new ObservableCollection<DataPointViewModel> { new DataPointViewModel {X = 1, Y = 3},  new DataPointViewModel {X = 2, Y = 1}}}
                         };
-            Data = new ObservableCollection<DataPointViewModel>();
-            
-            Refresh = new DelegateCommand();
 
-            OnInitialize();
+            Columns = new ObservableCollection<DataSetViewModel>
+                        {
+                            new DataSetViewModel {Name = "Test3", Data = new ObservableCollection<DataPointViewModel> { new DataPointViewModel {X = 1, Y = 3},  new DataPointViewModel {X = 2, Y = 1}}},
+                            new DataSetViewModel {Name = "Test4", Data = new ObservableCollection<DataPointViewModel> { new DataPointViewModel {X = 1, Y = 5},  new DataPointViewModel {X = 2, Y = 2}}}
+                        };
+
+            Areas = new ObservableCollection<DataSetViewModel>
+                        {
+                            new DataSetViewModel {Name = "Test5", Data = new ObservableCollection<DataPointViewModel> { new DataPointViewModel {X = 1, Y = 6},  new DataPointViewModel {X = 2, Y = 4}}},
+                            new DataSetViewModel {Name = "Test6", Data = new ObservableCollection<DataPointViewModel> { new DataPointViewModel {X = 1, Y = 5},  new DataPointViewModel {X = 2, Y = 2}}}
+                        };
+
+           
+            Refresh = new DelegateCommand();
             
             ApplyConvention(new BindCommandsDelegatesToMethods());
-            
-            
+
         }
 
-        partial void OnInitialize();
     }
 }
