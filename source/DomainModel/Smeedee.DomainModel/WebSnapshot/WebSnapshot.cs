197931e0dba90115743f2fc834865016f80422a3
Aleksander S. Larsen <aleksander@ogrim.no>
2011-07-18 11:36:05 +0200

    Updated DomainModel with new fields

--- source/DomainModel/Smeedee.DomainModel/WebSnapshot/WebSnapshot.cs
+++ source/DomainModel/Smeedee.DomainModel/WebSnapshot/WebSnapshot.cs
@@ -7,9 +7,14 @@ namespace Smeedee.DomainModel.WebSnapshot
 {
     public class WebSnapshot
     {
+        public virtual string Name { get; set; }
         public virtual byte[] Picture { get; set; }
         public virtual int PictureHeight { get; set; }
         public virtual int PictureWidth { get; set; }
-        public virtual int[] CropCoordinates { get; set; }
+
+        public virtual int CropHeight { get; set; }
+        public virtual int CropWidth { get; set; }
+        public virtual int CropCoordinateX { get; set; }
+        public virtual int CropCoordinateY { get; set; }
     }
 }
