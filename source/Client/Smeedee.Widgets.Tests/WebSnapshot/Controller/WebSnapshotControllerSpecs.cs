661ad248cd19404e57ef10439a6ec4e6bf9fd706
Mads Tordal <mads.tordal@gmail.com>
2011-07-27 10:07:56 +0200

    Moving WebSnapshotController from SL project and linking it in. Doing a manual merge from old to new file.

--- source/Client/Smeedee.Widgets.Tests/WebSnapshot/Controller/WebSnapshotControllerSpecs.cs
+++ source/Client/Smeedee.Widgets.Tests/WebSnapshot/Controller/WebSnapshotControllerSpecs.cs
@@ -1,35 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using Moq;
 using NUnit.Framework;
+using Smeedee.Client.Framework.Services;
+using Smeedee.DomainModel.Config;
+using Smeedee.DomainModel.Framework;
+using Smeedee.DomainModel.Framework.Logging;
+using Smeedee.Widgets.WebSnapshot.Controllers;
+using Smeedee.Widgets.WebSnapshot.ViewModel;
 using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.Specification.NUnit;
+using TinyMVVM.Framework.Services;
 
 namespace Smeedee.Widgets.Tests.WebSnapshot.Controller
 {
     public class Shared : ScenarioClass
-    { 
+    {
+        protected static WebSnapshotController controller;
+        protected static WebSnapshotViewModel viewModel;
+        protected static WebSnapshotSettingsViewModel settingsViewModel;
+        protected static Configuration config;
+        protected static Mock<IInvokeBackgroundWorker<IEnumerable<DomainModel.WebSnapshot.WebSnapshot>>> backgroundWorker;
+        protected static Mock<ITimer> timer;
+        protected static Mock<ILog> logger;
+        protected static Mock<IUIInvoker> uiInvoker;
+        protected static Mock<IProgressbar> progressbar;
+        protected static Mock<IPersistDomainModelsAsync<Configuration>> configPersister;
+        protected static Mock<IRepository<Smeedee.DomainModel.WebSnapshot.WebSnapshot>> repository;
+
+        protected Context controller_is_created = () =>
+                                                      {
+                                                          controller = new WebSnapshotController(viewModel, settingsViewModel, config, backgroundWorker.Object, timer.Object, logger.Object, uiInvoker.Object, progressbar.Object, configPersister.Object, repository.Object);
+                                                      };
+
+        protected When OnReset_is_called = () => settingsViewModel.Reset.ExecuteDelegate();
 
         [SetUp]
         public void SetUp()
         {
             Scenario("");
+             
+            viewModel = new WebSnapshotViewModel();
+            settingsViewModel = new WebSnapshotSettingsViewModel();
+            config = new Configuration();
+            backgroundWorker = new Mock<IInvokeBackgroundWorker<IEnumerable<DomainModel.WebSnapshot.WebSnapshot>>>();
+            timer = new Mock<ITimer>();
+            logger = new Mock<ILog>();
+            uiInvoker = new Mock<IUIInvoker>();
+            progressbar = new Mock<IProgressbar>();
+            configPersister = new Mock<IPersistDomainModelsAsync<Configuration>>();
+            repository = new Mock<IRepository<DomainModel.WebSnapshot.WebSnapshot>>();
+
         }
         [TearDown]
         public void TearDown()
         {
             StartScenario();
         }
-        
+
     }
     [TestFixture]
     public class When_Spawned : Shared
     {
+        [Test]
+        public void assure_controller_is_created()
+        {
+            Given(controller_is_created);
+            When("");
+            Then("controller is not null", () => controller.ShouldNotBeNull());
+        }
 
         [Test]
+        public void Then_assure_timer_is_started()
+        {
+            Given(controller_is_created);
+            When("");
+            Then("the timer should have started",
+                 () => timer.Verify(t => t.Start(It.IsAny<int>()), Times.Once()));
+        }
+
+        [Test]
+        public void assure_OnReset_resets_image()
+        {
+            Given(controller_is_created);
+            When(OnReset_is_called);
+            Then("image is reset", () => settingsViewModel.Image.ShouldBeNull());
+        }
+
+        [Test]
+        [Ignore]
         public void assure_we_get_data_from_repo()
         {
-            
+
         }
     }
 }
--- source/Client/Smeedee.Widgets/WebSnapshot/Controllers/WebSnapshotController.cs
+++ source/Client/Smeedee.Widgets/WebSnapshot/Controllers/WebSnapshotController.cs
@@ -1,52 +1,213 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Windows.Media.Imaging;
+using Smeedee.Client.Framework.Controller;
 using Smeedee.Client.Framework.Services;
 using Smeedee.DomainModel.Config;
+using Smeedee.DomainModel.Framework;
+using Smeedee.DomainModel.Framework.Logging;
+using Smeedee.DomainModel.WebSnapshot;
 using Smeedee.Framework;
 using Smeedee.Widgets.WebSnapshot.ViewModel;
+using TinyMVVM.Framework.Services;
 
 namespace Smeedee.Widgets.WebSnapshot.Controllers
 {
-    public class WebSnapshotController
+    public class WebSnapshotController : ControllerBase<WebSnapshotViewModel>
     {
         private WebSnapshotViewModel webSnapshotViewModel;
         private WebSnapshotSettingsViewModel webSnapshotSettingsViewModel;
+        private readonly IPersistDomainModelsAsync<Configuration> configPersisterRepository;
         private Configuration config;
-        private ITimer timer;
+        private IRepository<DomainModel.WebSnapshot.WebSnapshot> repository;
+        private IInvokeBackgroundWorker<IEnumerable<DomainModel.WebSnapshot.WebSnapshot>> asyncClient;
+        private ILog logger;
 
-        private const string refresh_interval = "refresh-interval";
-        private const string url = "url";
+        private const string url = "webSnaphotURL";
+        private const string refresh_interval = "webSnapshotRefreshInterval";
 
-        public WebSnapshotController(WebSnapshotViewModel webSnapshotViewModel, WebSnapshotSettingsViewModel webSnapshotSettingsViewModel, Configuration configuration, ITimer timer)
+        public WebSnapshotController(
+            WebSnapshotViewModel webSnapshotViewModel,
+            WebSnapshotSettingsViewModel webSnapshotSettingsViewModel,
+            Configuration configuration,
+            IInvokeBackgroundWorker<IEnumerable<DomainModel.WebSnapshot.WebSnapshot>> asyncClient,
+            ITimer timer,
+            ILog logger,
+            IUIInvoker uiInvoker,
+            IProgressbar loadingNotifier,
+            IPersistDomainModelsAsync<Configuration> configPersister,
+            IRepository<DomainModel.WebSnapshot.WebSnapshot> repository
+            )
+            : base(webSnapshotViewModel, timer, uiInvoker, loadingNotifier)
         {
             Guard.Requires<ArgumentNullException>(webSnapshotViewModel != null);
             Guard.Requires<ArgumentNullException>(webSnapshotSettingsViewModel != null);
             Guard.Requires<ArgumentNullException>(configuration != null);
-            Guard.Requires<ArgumentNullException>(timer != null);
+            //Guard.Requires<ArgumentNullException>(repository != null);
+
 
             config = configuration;
+            this.configPersisterRepository = configPersister;
             this.webSnapshotViewModel = webSnapshotViewModel;
+            this.logger = logger;
             this.webSnapshotSettingsViewModel = webSnapshotSettingsViewModel;
-            this.timer = timer;
+            this.repository = repository;
+            this.asyncClient = asyncClient;
+            webSnapshotSettingsViewModel.Save.ExecuteDelegate += OnSave;
+            webSnapshotSettingsViewModel.Reset.ExecuteDelegate += OnReset;
 
-            this.webSnapshotSettingsViewModel.FetchAsImage.ExecuteDelegate = () => FetchImage();
-            this.webSnapshotSettingsViewModel.FetchAsSnapshot.ExecuteDelegate = () => FetchSnapshot();
+
+            Start();
+            LoadData();
         }
 
-        private void FetchSnapshot()
+        private void OnSave()
         {
-            throw new NotImplementedException();
+
+
+            //configPersisterRepository.Save(config);
+
         }
 
-        private void FetchImage()
+        private void OnReset()
         {
-            throw new NotImplementedException();
+            uiInvoker.Invoke(() =>
+            {
+                //TODO Get(SelectedImage) and set it as Image
+                webSnapshotSettingsViewModel.Image =
+                    GenerateWriteableBitmap(
+                        "http://www.dvo.com/newsletter/monthly/2007/september/images/strawberry.jpg");
+            });
         }
 
+        private WriteableBitmap GenerateWriteableBitmap(string path)
+        {
+            var uri = new Uri(path);
+            var bmi = new BitmapImage(uri);
+            var wb = new WriteableBitmap(bmi);
 
+            uri = null;
+            bmi = null;
 
+            return wb;
+        }
+
+        protected void LoadData()
+        {
+            LoadData(new WebSnapshotSpecification());
+        }
+
+        protected void LoadData(Specification<DomainModel.WebSnapshot.WebSnapshot> specification)
+        {
+            if (!ViewModel.IsLoading)
+            {
+                asyncClient.RunAsyncVoid(() => LoadDataSync(specification));
+            }
+        }
+
+        private void LoadDataSync(Specification<DomainModel.WebSnapshot.WebSnapshot> specification)
+        {
+            SetIsLoadingData();
+            var snapshot = QuerySnapshot(specification);
+            logger.WriteEntry(new LogEntry("Snapshot", "I got the snapshot" + snapshot.ToString()));
+
+            uiInvoker.Invoke(() =>
+            {
+
+                try
+                {
+                    //logger.WriteEntry(new LogEntry("WebSnapshotController", "Tries to get snapshot from repository"));
+                    //var specification = new WebSnapshotSpecification();
+                    //logger.WriteEntry(new LogEntry("Specification", "I maded the specification"+specification.ToString()));
+                    //var snapshot = repository.Get(new WebSnapshotSpecification());
+                    //logger.WriteEntry(new LogEntry("repository", "repository is lame "+repository.GetType() + " - " + repository.ToString()));
+                    //var snapshot = repository.Get(specification);
+
+                    UpdateViewModel(snapshot);
+                }
+                catch (Exception e)
+                {
+                    LogErrorMsg(e);
+                    ViewModel.HasConnectionProblems = true;
+                }
+
+            });
+            SetIsNotLoadingData();
+        }
+
+        protected IEnumerable<DomainModel.WebSnapshot.WebSnapshot> QuerySnapshot(Specification<DomainModel.WebSnapshot.WebSnapshot> specification)
+        {
+            IEnumerable<DomainModel.WebSnapshot.WebSnapshot> snapshot = null;
+            try
+            {
+                logger.WriteEntry(new LogEntry("QuerySnapshot", "QuerySnapshot called, im in teh try"));
+                logger.WriteEntry(new LogEntry("QuerySnapshot", "the repo is lame " + repository));
+                snapshot = repository.Get(specification);
+                logger.WriteEntry(new LogEntry("schnappy", "I got the snapshot"));
+                ViewModel.HasConnectionProblems = false;
+            }
+            catch (Exception e)
+            {
+                LogErrorMsg(e);
+                ViewModel.HasConnectionProblems = true;
+            }
+
+            return snapshot;
+        }
+
+        private void UpdateViewModel(IEnumerable<DomainModel.WebSnapshot.WebSnapshot> snapshots)
+        {
+            logger.WriteEntry(new LogEntry("UpdateViewModel", "UpdateViewModel called"));
+            if (snapshots.Count() > 0)
+            {
+                var snapshot = snapshots.First();
+                uiInvoker.Invoke(() =>
+                {
+                    var snapshotPath = snapshot.PictureFilePath;
+                    SetWebSnapshot(snapshotPath);
+                });
+            }
+            else
+            {
+                uiInvoker.Invoke(() => webSnapshotViewModel.HasStoredImage = false);
+            }
+        }
+
+        private void SetWebSnapshot(string imagePath)
+        {
+            webSnapshotViewModel.Snapshot = GenerateWriteableBitmap(imagePath);
+            webSnapshotViewModel.HasStoredImage = imagePath != null;
+        }
+
+        //public void UpdateConfiguration(Configuration config)
+        //{
+        //    Guard.Requires<ArgumentNullException>(config != null);
+        //    Guard.Requires<ArgumentException>(config.ContainsSetting(url));
+        //    Guard.Requires<ArgumentException>(config.ContainsSetting(refresh_interval));
+
+        //    this.config = config;
+
+        //    webSnapshotSettingsViewModel.InputUrl = config.GetSetting(url).Value;
+        //}
+
+        //public Configuration SaveConfiguration()
+        //{
+        //    config.ChangeSetting(url, webSnapshotSettingsViewModel.InputUrl);
+        //    config.ChangeSetting(refresh_interval, webSnapshotSettingsViewModel.RefreshInterval.ToString());
+
+        //    return config;
+        //}
+
+        protected override void OnNotifiedToRefresh(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        protected void LogErrorMsg(Exception exception)
+        {
+            logger.WriteEntry(ErrorLogEntry.Create(this, exception.ToString()));
+        }
 
         public static Configuration GetDefaultConfiguration()
         {
@@ -66,13 +227,13 @@ namespace Smeedee.Widgets.WebSnapshot.Controllers
 
             this.config = config;
 
-            webSnapshotViewModel.InputUrl = config.GetSetting(url).Value;
+            //webSnapshotViewModel.InputUrl = config.GetSetting(url).Value;
         }
 
         public Configuration SaveConfiguration()
         {
-            config.ChangeSetting(url, webSnapshotViewModel.InputUrl);
-            config.ChangeSetting(refresh_interval, webSnapshotViewModel.RefreshInterval.ToString());
+            //config.ChangeSetting(url, webSnapshotViewModel.InputUrl);
+            //config.ChangeSetting(refresh_interval, webSnapshotViewModel.RefreshInterval.ToString());
 
             return config;
         }
