829006fe4e0d6cf30f0c1a946a7593f3df0a86d6
Dag Olav Prestegarden <dagolav@prestegarden.com>
2011-07-28 09:55:51 +0200

    Apikeys are now alphanumeric only. Refactoring authenticatin method to its own class that can be used by all the services

--- source/Client/Smeedee.Client.Web/MobileServices/Auth/Default.aspx.cs
+++ source/Client/Smeedee.Client.Web/MobileServices/Auth/Default.aspx.cs
@@ -1,38 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using Smeedee.DomainModel.Config;
-using Smeedee.DomainModel.Framework;
-using Smeedee.Integration.Database.DomainModel.Repositories;
+using Smeedee.Client.Web.Framework;
 
 namespace Smeedee.Client.Web.MobileServices.Auth
 {
     public partial class Default : System.Web.UI.Page
     {
-        private IRepository<Configuration> configRepo = new ConfigurationDatabaseRepository();
+        private readonly MobileServicesAuthenticator authenticator = new MobileServicesAuthenticator();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             var apiKey = Request.QueryString["apiKey"] ?? "";
-            Response.Write(IsApiKeyValid(apiKey));
-        }
-
-        private bool IsApiKeyValid(string apiKey)
-        {
-            var config = configRepo.Get(new ConfigurationByName("MobileServices"));
-            if (config != null && config.Count() > 0)
-            {
-                if (config.First().ContainsSetting("ApiKey"))
-                {
-                    Response.Write("Actual: " + config.First().GetSetting("ApiKey").Value + " Sent in: " + apiKey);
-                    return apiKey == config.First().GetSetting("ApiKey").Value;
-                    
-                }
-            }
-            return false;
+            Response.Write(authenticator.IsApiKeyValid(apiKey));
         }
     }
 }
