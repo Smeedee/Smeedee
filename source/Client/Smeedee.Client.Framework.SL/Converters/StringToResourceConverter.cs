691ffd4e12751477b24319ab088106dfa0332838
Kristian Hiim <kristian.hiim@gmail.com>
2011-07-28 11:54:02 +0200

    Provided a generic BrushProvider. Will not make a more generic color chooser, as this is enough. Closes #59

---
+++ source/Client/Smeedee.Client.Framework.SL/Converters/StringToResourceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace Smeedee.Client.Framework.SL.Converters
+{
+    public class StringToResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Application.Current.Resources[value.ToString()];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/Client/Smeedee.Client.Framework/Resources/BrushProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smeedee.Client.Framework.Resources
+{
+    public class BrushProvider
+    {
+        public const string DEFAULT_BRUSH = "GreyGradientBrush";
+       
+        public List<BrushProviderItem> BrushList
+        {
+            get
+            {
+                var list = new List<BrushProviderItem>();
+                foreach (var key in GetBrushKeys())
+                {
+                    list.Add(new BrushProviderItem(key, GetBrushName(key)));
+                }
+                return list;
+            }
+        }
+
+        private static Dictionary<string, string> brushes = new Dictionary<string, string>
+        {   
+            {"grey", "DarkGreyGradientBrush"},
+            {"darkGrey", "GreyGradientBrush"},
+            {"lightGrey", "LightGreyGradientBrush"},
+
+            {"lightBrown", "LightBrownGradientBrush"},
+            {"brown", "BrownGradientBrush"},
+            {"darkBrown", "DarkBrownGradientBrush"},
+
+            {"red", "RedGradientBrush"},
+            {"orange", "OrangeGradientBrush"},
+            {"yellow", "YellowGradientBrush"},
+
+            {"lightGreen", "LightGreenGradientBrush"},
+            {"green", "MediumGreenGradientBrush"},
+            {"darkGreen", "GreenGradientBrush"},
+
+            {"lightBlue", "LightBlueGradientBrush"},
+            {"blue", "BlueGradientBrush"},
+            {"darkBlue", "DarkBlueGradientBrush"},
+          
+            {"pink", "PinkGradientBrush"},
+            {"purple", "PurpleGradientBrush"},
+            {"darkPurple", "DarkPurpleGradientBrush"}
+          
+        };
+
+        public static string GetBrushName(string color)
+        {
+            return brushes.ContainsKey(color) ? brushes[color] : DEFAULT_BRUSH;
+        }
+
+        public static string[] GetBrushKeys()
+        {
+            return brushes.Keys.ToArray();
+        }
+    }
+
+    public class BrushProviderItem
+    {
+        public BrushProviderItem(string name, string brush)
+        {
+            BrushKey = name;
+            BrushName = brush;
+        }
+        public string BrushKey { get; set; }
+        public string BrushName { get; set; }
+    }
+
+}
