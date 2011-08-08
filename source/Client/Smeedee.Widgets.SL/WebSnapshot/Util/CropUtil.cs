956e3f5d8c4bf0fea896601a0557370a8af0a434
Aleksander S. Larsen <aleksander@ogrim.no>
2011-08-08 14:43:52 +0200

    Refactored method in CropUtil, removede unnecessary parameter and simplified the code

--- source/Client/Smeedee.Widgets.SL/WebSnapshot/Util/CropUtil.cs
+++ source/Client/Smeedee.Widgets.SL/WebSnapshot/Util/CropUtil.cs
@@ -8,50 +8,25 @@ namespace Smeedee.Widgets.SL.WebSnapshot.Util
 {
     public static class CropUtil
     {
-        public static Point GetUpperLeftCornerInRectangle(Point press, Point release, Rectangle rectangle)
+        public static Point GetUpperLeftCornerInRectangle(Point press, Point release)
         {
-            var upperLeft  = new Point(0,0);
+            if (press.X < release.X && press.Y < release.Y)
+                return press;
             
-            if(press.X.CompareTo(release.X) < 0)
-            {
-                if(press.Y.CompareTo(release.Y) < 0)
-                {
-                    upperLeft = press;
-                }
-                else
-                {
-                    upperLeft.X = press.X;
-                    upperLeft.Y = press.Y - rectangle.Height;
-                }
-            }
-            else
-            {
-                if(press.X.CompareTo(release.X) < 0)
-                {
-                    upperLeft.X = release.X - rectangle.Width;
-                    upperLeft.Y = release.Y - rectangle.Height;
-                }
-                else
-                {
-                    if (press.Y.CompareTo(release.Y) < 0)
-                    {
-                        upperLeft.X = release.X;
-                        upperLeft.Y = release.Y - rectangle.Height; 
-                    }
-                    else
-                    {
-                        upperLeft = release;
-                    }
-                    
-                }
-            }
-            return upperLeft;
+            if (release.X < press.X && release.Y < press.Y)
+                return release;
+            
+            if (press.X > release.X && press.Y < release.Y)
+                return new Point(release.X, press.Y);
+            
+            return new Point(press.X, release.Y);
         }
 
         public static bool OutsidePicture(Point position, Image img)
         {
             return position.Y > img.ActualHeight || position.X > img.ActualWidth || position.X < 0 || position.Y < 0;
         }
+
         public static double NegativeNumber(double number)
         {
             return -Math.Abs(number);
@@ -68,13 +43,11 @@ namespace Smeedee.Widgets.SL.WebSnapshot.Util
                 return null;
             }
 
-            // Clamp to boundaries
             if (x < 0) x = 0;
             if (x + width > srcWidth) width = srcWidth - x;
             if (y < 0) y = 0;
             if (y + height > srcHeight) height = srcHeight - y;
 
-            // Copy the pixels line by line using fast BlockCopy
             var result = new WriteableBitmap(width, height);
             for (var line = 0; line < height; line++)
             {
