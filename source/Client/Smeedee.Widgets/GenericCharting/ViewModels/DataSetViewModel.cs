b3dc5befa350334419c039ce97fdcee547690d05
Kristian Hiim <kristian.hiim@gmail.com>
2011-08-08 15:01:37 +0200

    Updated Generic Charting to remove some memory leaks. #65

--- source/Client/Smeedee.Widgets/GenericCharting/ViewModels/DataSetViewModel.cs
+++ source/Client/Smeedee.Widgets/GenericCharting/ViewModels/DataSetViewModel.cs
@@ -1,16 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.ObjectModel;
+using Smeedee.Client.Framework.ViewModel;
 
 namespace Smeedee.Widgets.GenericCharting.ViewModels
 {
-    public partial class DataSetViewModel
+    public class DataSetViewModel : BasicViewModel
     {
-        partial void OnInitialize()
+        public DataSetViewModel()
         {
             Data = new ObservableCollection<DataPointViewModel>();
+
         }
+        public virtual string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+
+        private string _Name;
+
+        public ObservableCollection<DataPointViewModel> Data
+        {
+            get
+            {
+                return _Data;
+            }
+            set
+            {
+                _Data = value;
+            }
+        }
+
+        private ObservableCollection<DataPointViewModel> _Data;
+
+        public virtual string Type
+        {
+            get
+            {
+                return _Type;
+            }
+            set
+            {
+                _Type = value;
+            }
+        }
+
+        private string _Type;
+ 
+        public virtual string Brush
+        {
+            get
+            {
+
+                return _Brush;
+            }
+            set
+            {
+                if (value != _Brush)
+                {
+                    _Brush = value;
+                    TriggerPropertyChanged("Brush");
+                }
+            }
+        }
+        private string _Brush;
+
+
+
+ 
     }
 }
