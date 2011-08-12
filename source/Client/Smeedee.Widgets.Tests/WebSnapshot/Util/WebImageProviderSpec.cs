fc981efb1600ab2161b11962ec9560c531ddc052
Hege Augland <hege.augland@capgemini.no>
2011-08-12 09:46:30 +0200

    Finished testing websnapshot provider #51

--- source/Client/Smeedee.Widgets.Tests/WebSnapshot/Util/WebImageProviderSpec.cs
+++ source/Client/Smeedee.Widgets.Tests/WebSnapshot/Util/WebImageProviderSpec.cs
@@ -13,53 +13,110 @@ namespace Smeedee.Widgets.Tests.WebSnapshot.Util
     internal class WebImageProviderSpec
     {
         [TestFixture]
-        public class When_using_WebImageProvider : Shared
+        public class When_getting_bitmap_from_url : Shared
         {
-            private string url;
-            private string xpath;
-            private Bitmap aBitmap;
-            private string pictureNodeUrl;
+            private static Bitmap gotBitmap;
 
             [Test]
-            [Ignore]
-            public void Assure_get_bitmap_from_url_returns_null_when_url_is_random_string()
+            public void Assure_returns_bitmap_when_url_is_not_a_picture_url()
             {
                 Given("url is a random string", () => url = "aURL");
-                When("getting bitmap from url", () => aBitmap = webImageProvider.GetBitmapFromURL(url));
-                Then("should return null", () => aBitmap.ShouldBeNull());
+                When(getting_bitmap_from_url);
+                Then("should return null", () =>
+                {
+                    gotBitmap.ShouldBeInstanceOfType<Bitmap>();
+                    gotBitmap.ShouldNotBeNull();
+                });
             }
 
             [Test]
-            public void Assure_returns_null_when_get_picture_node_url_from_xpath_when_url_and_xpath_are_random_string()
+            public void Assure_returns_null_when_url_is_an_empty_image_url()
+            {
+                Given("url is a random string", () => url = emptyImageURL);
+                When(getting_bitmap_from_url);
+                Then("should return null", () => gotBitmap.ShouldBeNull());
+            }
+
+            [Test]
+            public void Assure_returns_bitmap_when_url_is_a_picture_url()
+            {
+                Given("url is a picture url", () => url = imageURL);
+                When(getting_bitmap_from_url);
+                Then("should return", () =>
+                {
+                    gotBitmap.ShouldBeInstanceOfType<Bitmap>();
+                    gotBitmap.ShouldNotBeNull();
+                });
+            }
+            private When getting_bitmap_from_url = () => gotBitmap = webImageProvider.GetBitmapFromURL(url);
+        }
+
+        [TestFixture]
+        public class When_getting_picture_node_url_from_xpath_and_url : Shared
+        {
+            private static string gotPictureNodeUrl;
+
+            [Test]
+            public void Assure_returns_null_when_url_and_xpath_are_random_string()
             {
                 Given("url and xpath are random strings", () =>
-                                                              {
-                                                                  url = "aURL";
-                                                                  xpath = "aXpath";
-                                                              });
-                When("getting picture node from xpath",
-                     () => pictureNodeUrl = webImageProvider.GetPictureNodeURLFromXpath(url, xpath));
-                Then("should return null", () => pictureNodeUrl.ShouldBeNull());
+                {
+                    url = "aURL";
+                    xpath = "aXpath";
+                });
+                When(getting_picture_node_from_xpath);
+                Then("should return null", () => gotPictureNodeUrl.ShouldBeNull());
             }
+
+            [Test]
+            public void Assure_returns_null_when_xpath_is_random_string()
+            {
+                Given("url and xpath are random strings", () =>
+                {
+                    url = imageURL;
+                    xpath = "aXpath";
+                });
+                When(getting_picture_node_from_xpath);
+                Then("should return null", () => gotPictureNodeUrl.ShouldBeNull());
+            }
+
+            [Test]
+            public void Assure_returns_string_when_url_and_xpath_are_correct()
+            {
+                Given("url and xpath are random strings", () =>
+                {
+                    url = pageURL;
+                    xpath = xPath;
+                });
+                When(getting_picture_node_from_xpath);
+                Then("should return null", () => 
+                { 
+                    gotPictureNodeUrl.ShouldBeInstanceOfType<string>(); 
+                    gotPictureNodeUrl.ShouldNotBeNull(); 
+                });
+            }
+            private When getting_picture_node_from_xpath = () => gotPictureNodeUrl = webImageProvider.GetPictureNodeURLFromXpath(url, xpath);
         }
 
         public class Shared : ScenarioClass
         {
             protected static WebImageProvider webImageProvider;
 
+            protected static string url;
+            protected static string xpath;
             protected static string imageURL;
+            protected static string emptyImageURL;
+
             protected static string pageURL;
             protected static string xPath;
-            protected static string xPathParsed;
-
 
             [SetUp]
             public void SetUp()
             {
                 imageURL = "http://smeedee.org/images/code.png";
+                emptyImageURL = "blabla.png";
                 pageURL = "http://smeedee.org/";
                 xPath = "/html/body/div[2]/div[2]/div/div/div/img";
-                xPathParsed = "images/code.png/";
 
                 webImageProvider = new WebImageProvider();
                 Scenario("");
